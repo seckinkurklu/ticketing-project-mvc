9be5f1fa19acac63431d3125ad4af5ec1d7531d8
seckinkurklu <kurkluseckin@gmail.com>
2024-04-18 13:42:31 +0300

    Ticketing Project

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.UserDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/create")
-    public String createuser() {
+    public String createuser(Model model) {
+
+      model.addAttribute("user", new UserDTO());
+
         return "/user/create";
 
     }
