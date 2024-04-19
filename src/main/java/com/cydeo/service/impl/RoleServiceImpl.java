cac92ead171da845e424fb33678014ca5bc45546
seckinkurklu <kurkluseckin@gmail.com>
2024-04-19 14:32:03 +0300

    Ticketing Project

---
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -0,0 +1,29 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
+
+import java.util.List;
+
+public class RoleServiceImpl extends AbstractMapService<RoleDTO, Long> implements RoleService {
+
+    @Override
+    public RoleDTO save(RoleDTO user) {
+        return null;
+    }
+
+    @Override
+    public RoleDTO findById(Long username) {
+        return null;
+    }
+
+    @Override
+    public List<RoleDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long username) {
+
+    }
+}
