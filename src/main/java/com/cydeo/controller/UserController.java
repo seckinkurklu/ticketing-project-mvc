02d2e5d8e862911fb7e1dbd11a2f597ce00cae99
seckinkurklu <kurkluseckin@gmail.com>
2024-05-07 15:57:43 +0300

    Ticketing Project

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,22 +1,45 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+
     @GetMapping("/create")
-    public String createuser(Model model) {
+    public String createUser(Model model) {
 
       model.addAttribute("user", new UserDTO());
-      model.addAttribute("roles");
+      model.addAttribute("roles", roleService.findAll());
+      model.addAttribute("users", userService);
 
         return "/user/create";
+
+    }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user") UserDTO user){
+
+        userService.save(user);
+
+        return "redirect:/user/create";
 
     }
 
