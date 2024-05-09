7546d5e321ec55ec472010a568fc56c9042fa404
seckinkurklu <kurkluseckin@gmail.com>
2024-05-09 14:25:40 +0300

    Ticketing Project

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,10 +5,7 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -43,9 +40,10 @@ public class UserController {
 
     }
 
-    public String editUser(Model model) {
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model) {
 
-model.addAttribute("user", )
+        model.addAttribute("user", userService.findById(username));
 
         return "/user/update";
     }
