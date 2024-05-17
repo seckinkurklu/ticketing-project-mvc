860a52652300ef80a2d6e8b0a649767fd1992239
seckinkurklu <kurkluseckin@gmail.com>
2024-05-17 22:44:52 +0300

    Ticketing Project

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProjectController {
 
     @GetMapping("/create")
-    public String createProject(){
+    public String createProject(Model model) {
 
+        model.addAttribute("project", new ProjectDTO());
 
+        model.addAttribute("manager", )
 
         return "/project/create";
     }
