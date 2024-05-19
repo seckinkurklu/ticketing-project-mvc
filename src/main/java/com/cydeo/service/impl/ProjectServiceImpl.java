da7d48433422be488cc0521aa0d31a5124a9bb52
seckinkurklu <kurkluseckin@gmail.com>
2024-05-19 13:59:48 +0300

    Ticketing Project

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,10 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> i
 
     @Override
     public ProjectDTO save(ProjectDTO project) {
+
+        if (project.getProjectStatus() == null)
+            project.setProjectStatus(Status.OPEN);
+
         return super.save(project.getProjectCode(), project);
     }
 
